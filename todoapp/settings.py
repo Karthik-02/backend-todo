c31aca32cc6089936be98007f10ece91b2fdc4e9
Karthik S <81423983+Karthik-02@users.noreply.github.com>
2023-05-30 20:59:30 +0530

    Update settings.py

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -90,10 +90,6 @@ WSGI_APPLICATION = "todoapp.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
 }
 
 
